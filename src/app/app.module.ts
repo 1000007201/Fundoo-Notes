bd0768fb3c643c06cb5709f68dfa9eb397bd67a3
1000007201 <nishant.sharma5504@gmail.com>
2022-06-24 22:50:09 +0530

    [Nishant] Added:Forgot password componemt and created form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    ForgetPasswordComponent
   ],
   imports: [
     BrowserModule,
