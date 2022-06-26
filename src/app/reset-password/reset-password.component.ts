b4e0baf6ef5d4ce6626812ef93f793a24c7b0524
1000007201 <nishant.sharma5504@gmail.com>
2022-06-27 00:57:27 +0530

    [Nishant] Added: Some validations in Reset password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
+  resetForm !: FormGroup;
+  submitted=false;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.resetForm=this.fb.group({
+      newpassword: ['', Validators.required],
+      confirmpassword: ['', Validators.required]
+    })
+  }
+  get f(){ return this.resetForm.controls;}
+  onSubmit(){
+    this.submitted=true;
+
+    if (this.resetForm.invalid){
+      return;
+    }
+    console.log(this.resetForm.value);
   }
 
 }
